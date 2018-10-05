21625334da29d95a8cc405b93facc19d70912f0a
Jack Crymble <j.crymble@analyticsengines.com>
2018-10-05 09:05:08 +0100

    Reproduction of error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import * as dctable from 'dc.datatables/dc.datatables';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'reproduction';
+
+  constructor() {
+    console.log(dctable);
+  }
+
 }
